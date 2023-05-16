fca06274652cc6a61cf0aa3f79c7d3fd74bb7d8d
darksidebooks <50843970+darksidebooks@users.noreply.github.com>
2023-05-15 22:23:48 -0300

    agora foi

--- forms/script.js
+++ forms/script.js
@@ -1,17 +1,20 @@
 const form = document.querySelector('form');
 const inputs = form.querySelectorAll('input');
+const errorMessage = document.getElementById('error-message');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   let isValid = true;
 
   inputs.forEach((input) => {
-    if (input.type === 'email') {
-      // Validate email field
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(input.value)) {
+    if (input.id === 'celular') {
+      // Validate celular field
+      const celularRegex = /^\d{11}$/; // Update regex to match 11 digits
+      if (!celularRegex.test(input.value)) {
         isValid = false;
         input.classList.add('error');
+        errorMessage.textContent = 'O celular deve ter 11 dígitos';
+        errorMessage.style.display = 'block'; // Display error message
       } else {
         input.classList.remove('error');
       }
@@ -25,6 +28,16 @@ form.addEventListener('submit', (event) => {
   });
 
   if (isValid) {
+    // Submit form and redirect to success page
     form.submit();
+    window.location.href = 'success.html';
+  }
+});
+
+const celularField = document.getElementById('celular');
+celularField.addEventListener('keypress', (event) => {
+  const key = event.key;
+  if (isNaN(key)) {
+    event.preventDefault();
   }
 });
